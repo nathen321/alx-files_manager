458c9387fe87f5e6eb8c3edf876f185f8fa2a799
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-24 09:57:42 +0100

    feat: refactor Redis client implementation and add main application logic

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,52 @@
-const redis = require('redis');
+import { createClient } from 'redis';
 
-// Create a client
-const client = redis.createClient({
-  url: 'redis://localhost:6379',
-});
+class RedisClient {
+  constructor() {
+    this.client = createClient({
+      url: 'redis://localhost:6379',
+    }).on('error', (error) => {
+      console.log(error);
+    });
+  }
 
-module.exports = client;
+  isAlive() {
+    try {
+      this.client.connect();
+      return this.client.ping() === 'PONG';
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async get(key) {
+    try {
+      await this.client.connect();
+      const value = await this.client.get(key);
+      return value;
+    } catch (err) {
+      console.log('Redis Client Error', err);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.client.connect();
+      await this.client.setEx(key, duration, JSON.stringify(value));
+    } catch (err) {
+      console.log('Redis Client Error', err);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.connect();
+      await this.client.del(key);
+    } catch (err) {
+      console.log('Redis Client Error', err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;

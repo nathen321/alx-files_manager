533eef5587c5d4bd52274c8641b5722addc6a9c9
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-24 10:51:27 +0100

    fix: await redisClient.isAlive() for proper asynchronous handling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import redisClient from './utils/redis';
 
 (async () => {
-  console.log(redisClient.isAlive());
+  console.log(await redisClient.isAlive());
   console.log(await redisClient.get('myKey'));
   await redisClient.set('myKey', 12, 5);
   console.log(await redisClient.get('myKey'));
